c642c2fd99fe8ff71bdbc9de58106354c3a3cc03
Ram Chavali <32201301+ramlohith@users.noreply.github.com>
2019-09-02 19:43:59 -0400

    Add files via upload

---
+++ ruby_intro_19fall.rb
@@ -0,0 +1,30 @@
+
+  # Part 1
+
+  def two_sum(a,k)
+    # ADD YOUR CODE HERE
+  end
+
+  def sorted_squares(a)
+    # ADD YOUR CODE HERE
+  end
+
+  def unique_total(a)
+    # ADD YOUR CODE HERE
+  end
+
+# Part 2
+
+  def unique_char(s)
+    # ADD YOUR CODE HERE
+  end
+
+  def reverse_words(s)
+    # ADD YOUR CODE HERE
+  end
+
+# Part 3
+
+  class Movie
+    # ADD YOUR CODE HERE
+  end
